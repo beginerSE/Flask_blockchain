a421a004fbcf14eaeb338db748b649c1f0dcc1cf
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-11-07 14:37:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,40 @@ genesis_block_str = str(genesis_block['no']) \
 genesis_block_byte = genesis_block_str.encode('utf-8')
 genesis_block_hash = sha256(genesis_block_byte).hexdigest()
 genesis_block['hash'] = genesis_block_hash
+
+app = Flask(__name__)
+block_chain = []
+block_chain.append(genesis_block)
+nodes = []
+pool = []
+
+@app.route('/add_transaction', methods=['GET'])
+def add_transaction():
+    transaction = request.args.get('transaction')
+    pool.append(transaction)
+    return jsonify(pool)
+
+@app.route('/get_pool', methods=['GET'])
+def get_pool():
+    return jsonify(pool)
+
+@app.route('/remove_pool', methods=['GET'])
+def remove_pool():
+    transaction = request.args.get('transaction')
+    pool.remove(transaction)
+    return jsonify(pool)
+
+@app.route('/last_block', methods=['GET'])
+def last_block():
+    return jsonify(block_chain[-1])
+
+@app.route('/get_block', methods=['GET'])
+def get_block():
+    no = request.args.get('no')
+    if no and len(block_chain) > int(no):
+        block = block_chain[int(no)]
+    else:
+        block = {}
+    return jsonify(block)
+
+
